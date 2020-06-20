9eca49b20c592e4c048a3a7f56beb4be84d6e7cd
n1ki4 <glazunov_n@ukr.net>
2020-06-20 13:29:42 +0300

    add crud for manage comments

--- project/board/urls.py
+++ project/board/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-    path("posts/", views.PostListView.as_view(), name="post_list"),
-    path("posts/<int:pk>/", views.PostDetailView.as_view(), name="single_post"),
+    path("posts/", views.PostList.as_view(), name="post_list"),
+    path("posts/<int:pk>/", views.PostDetail.as_view(), name="single_post"),
     path("posts/<int:pk>/upvote/", views.PostUpvote.as_view(), name="post_upvote"),
-    path("posts/<int:pk>/comments/", views.CommentCreateView.as_view(), name="comment_create"),
+    path("comments/", views.Comments.as_view(), name="comment_create"),
+    path("comments/<int:pk>/", views.CommentDeleteUpdate.as_view(), name="change_comment"),
 ]
--- project/board/views.py
+++ project/board/views.py
@@ -1,8 +1,9 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Post
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from .models import Post, Comment
 from .serializers import (
     PostDetailSerializer,
     PostSerializer,
@@ -10,7 +11,9 @@ from .serializers import (
 )
 
 
-class PostListView(APIView):
+class PostList(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def post(self, request):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
@@ -26,7 +29,8 @@ class PostListView(APIView):
         return Response(serializer.data)
 
 
-class PostDetailView(APIView):
+# TODO IS AUTHOR OR READONLY
+class PostDetail(APIView):
     def get(self, request, pk):
         """get a single post"""
         post = Post.objects.get(id=pk)
@@ -49,21 +53,6 @@ class PostDetailView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CommentCreateView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, pk):
-        post = Post.objects.get(id=pk)
-        serializer = CommentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(
-                    author=self.request.user,
-                    post=post
-            )
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
 class PostUpvote(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -73,7 +62,28 @@ class PostUpvote(APIView):
             post.upvotes.remove(request.user)
             post.save()
             return Response({"Success": "Upvote has been deleted."}, status=status.HTTP_201_CREATED)
-        else:
-            post.upvotes.add(request.user)
-            post.save()
-            return Response({"Success": "Upvote has been added."}, status=status.HTTP_201_CREATED)
+        post.upvotes.add(request.user)
+        post.save()
+        return Response({"Success": "Upvote has been added."}, status=status.HTTP_201_CREATED)
+
+
+class Comments(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+# TODO IS AUTHOR OR READONLY
+class CommentDeleteUpdate(RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
